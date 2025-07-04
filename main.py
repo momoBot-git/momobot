856186ce2d75c94c54ee310acb73ea0f817ae509
momoBot-git <momomomomo@tutamail.com>
2025-07-04 13:04:20 +0700

    Update main.py
    CURRENTLY DOES NOT WORK
    cant login to pixiv will bugfix tmrw probably

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import logging
 from dotenv import load_dotenv 
 import os
 from pixivapi import Client
+from gppt import GetPixivToken
+
+client = Client()
 
 #TODO
 #determine if r18 and block sending in sfw channels if true
@@ -28,30 +31,48 @@ class Bot(commands.Bot): #define bot and scope
 
 #defining vars
 bot = Bot()
-client = Client()
-#---
 
-async def pixivImageGetter(id):
-  client.login("user_xhru3788", "severminecraft")
-  
+#ive entirely given up on having readable code
+def get_refresh_token() -> str:
+  with open("token.txt", "w+") as f:
+      if refresh_token := f.read().strip():
+          return refresh_token
+
+      g = GetPixivToken(headless=True)
+      refresh_token = g.login(username="user_xhru3788", password="severminecraft")["refresh_token"]
+      f.write(refresh_token)
+      return refresh_token
+
 
 #botloop
 @bot.event 
 async def on_ready(): 
   print(f'Logged in as {bot.user}') #prints to log ('logged in as momobot #whatever')
-
+  
 @bot.event
 async def on_message(message):
-    if message.author == bot.user: #*should* be temp
+    if message.author == bot.user:
         return
     if "gay" in message.content.lower():
         await message.channel.send(message.author.avatar)
         await message.channel.send(message.author.mention)      
+        client.authenticate(get_refresh_token()) #login to pixiv
+        print(f'Logged in to Pixiv: {client.account}')
+        await message.channel.send("logined")
+        return
 
 @bot.hybrid_command(name="test", with_app_command=True, description="Testes")
 @discord.app_commands.guilds(discord.Object(id=1390176544210550935)) #AZUREYAKO SERVER REPLACE
-async def test(ctx: commands.Context):
+async def test(ctx: commands.Context, member1: discord.Member, member2: discord.Member, id):
     #await ctx.defer(ephemeral=True)
-    await ctx.reply()
+    await ctx.send(id)
+    await ctx.send(member1.display_avatar.url)
+    await ctx.send(member2.display_avatar.url)
+    illustration = client.fetch_illustration(id)
+    await ctx.send(illustration.title)
+    for i in illustration.image_urls:
+      await ctx.send(str(i))
 
+print(get_refresh_token())
+print(f'Logged in to Pixiv: {client.account}')
 bot.run(token, log_handler = handler, log_level = logging.INFO) #yeah idk
